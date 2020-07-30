27a2ac7cda57cf5e8f5e1ad613f53e55d46eba2a
fermin <fyflzjz@aliyun.com>
2020-07-30 11:26:21 +0800

    init

--- includes/config.sample.inc.php
+++ includes/config.sample.inc.php
@@ -2,30 +2,16 @@
 $config = [
     'servers' => [
         [
-            'name' => '35',
+            'name' => 'name',
             'host' => '127.0.0.1',
             'port' => 6379,
             'auth' => '123456',
-//            'db' => 2,             // Optional database number, see http://redis.io/commands/select
-//            'databases' => 2,             // Optional number of databases (prevents use of CONFIG command).
-            'filter' => '*',
-            'scheme' => 'tcp', // Optional. Connection scheme. 'tcp' - for TCP connection, 'unix' - for connection by unix domain socket
-            'path' => '', // Optional. Path to unix domain socket. Uses only if 'scheme' => 'unix'. Example: '/var/run/redis/redis.sock'
+//            'db'        => 2, // 只能看几库
+//            'databases' => 5, // 只能看前几个库
+//            'filter'    => '*', // 筛选 [筛选值*]
+//            'scheme' => 'tcp',
+//            'path' => '',
         ],
-
-        /*array(
-          'name'      => 'local db 2',
-          'host'      => 'localhost',
-          'port'      => 6379,
-          'db'        => 1,             // Optional database number, see http://redis.io/commands/select
-          'databases' => 1,             // Optional number of databases (prevents use of CONFIG command).
-          'filter'    => 'something:*', // Show only parts of database for speed or security reasons.
-          'seperator' => '/',           // Use a different seperator on this database (default uses config default).
-          'flush'     => false,         // Set to true to enable the flushdb button for this instance.
-          'charset'   => 'cp1251',      // Keys and values are stored in redis using this encoding (default utf-8).
-          'keys'      => false,         // Use the old KEYS command instead of SCAN to fetch all keys for this server (default uses config default).
-          'scansize'  => 1000           // How many entries to fetch using each SCAN command for this server (default uses config default).
-        ),*/
     ],
 
 
